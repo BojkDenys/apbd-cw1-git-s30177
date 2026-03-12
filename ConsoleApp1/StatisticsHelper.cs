54cdcdab751eb27989baf4453471819e63a03d88
Denys Boiko <denisbojko2005@gmail.com>
2026-03-12 17:01:59 +0100

    add one more calculateaverage method

--- ConsoleApp1/StatisticsHelper.cs
+++ ConsoleApp1/StatisticsHelper.cs
@@ -2,8 +2,17 @@
 
 public class StatisticsHelper
 {
-    public static double CalculateAverage(int[] array)
+    public static double CalculateAverage1(int[] array)
     {
         return array.Average();
+    }
+    public static double CalculateAverage(int[] array)
+    {
+        int sum = 0;
+        for (int i = array.Length - 1; i >= 0; i--)
+        {
+            sum += array[i];
+        }
+        return sum / array.Length;
     }
 }
